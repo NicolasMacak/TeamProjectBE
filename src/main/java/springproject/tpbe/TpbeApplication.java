7725fc1d23afb5f6c3fd47ad69f7d05693bc81e4
kirschovapetra <kirschovapetra@gmail.com>
2021-02-04 14:57:12 -0500

    kukni ten vymakany readme

--- src/main/java/springproject/tpbe/TpbeApplication.java
+++ src/main/java/springproject/tpbe/TpbeApplication.java
@@ -2,22 +2,10 @@ package springproject.tpbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TpbeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TpbeApplication.class, args);
-    }
-
-    @RestController
-    static final class MyController {
-        
-        @GetMapping("/")
-        String hello(){
-            return "Hello darkness my old friend";
-        }
     }
 }
