11144fbf635655c34df49f3549356c413ec0a112
Nicolas <macak.nicolas@gmail.com>
2021-02-04 11:31:52 -0500

    init

---
+++ src/test/java/springproject/tpbe/TpbeApplicationTests.java
@@ -0,0 +1,13 @@
+package springproject.tpbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
